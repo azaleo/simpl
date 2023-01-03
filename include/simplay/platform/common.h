fb87683ff7878db4db26cbdbf105f3c7e8be3fb9
azaleo <amael.m@protonmail.com>
2023-01-03 16:05:57 +0100

    Diffuse reflections

--- include/simplay/platform/common.h
+++ include/simplay/platform/common.h
@@ -25,6 +25,39 @@ namespace sim {
     return min + (max-min)*random_f64();
   }
 
+  inline Vec3 random_vec3() {
+    f64 x = random_f64();
+    f64 y = random_f64();
+    f64 z = random_f64();
+    return Vec3(x, y, z);
+  }
+
+  inline Vec3 random_vec3_in(f64 min, f64 max) {
+    f64 x = random_f64_in(min, max);
+    f64 y = random_f64_in(min, max);
+    f64 z = random_f64_in(min, max);
+    return Vec3(x, y, z);
+  }
+
+  inline Vec3 random_vec3_in_unit_sphere() {
+    while (true) {
+      Vec3 p = random_vec3_in(-1.0, 1.0);
+      if (p.sqmag() < 1.0)
+        return p;
+    }
+    // Unreachable
+    return Vec3(0.0, 0.0, 0.0);
+  }
+
+  inline Vec3 random_vec3_in_hemisphere(const Vec3& normal) {
+    Vec3 in_sphere = random_vec3_in_unit_sphere();
+    return (dot(in_sphere, normal) > 0.0) ? in_sphere : -in_sphere;
+  }
+
+  inline Vec3 random_dir() {
+    return normalize(random_vec3_in_unit_sphere());
+  }
+
   inline f64 min(f64 a, f64 b) {
     return a < b ? a : b;
   }
--- modules/playground/src/main.cpp
+++ modules/playground/src/main.cpp
@@ -9,12 +9,18 @@ namespace sim {
   const f64 ASPECT_RATIO = 16.0 / 9.0;
   const i32 IMG_W = 400;
   const i32 IMG_H = (i32)(IMG_W / ASPECT_RATIO);
-  const i32 PIXEL_SAMPLES = 32;
+  const i32 PIXEL_SAMPLES = 100;
+  const i32 MAX_DEPTH = 10;
   
-  Color3 ray_color(const Ray& r, const Hittable& scene) {
+  Color3 ray_color(const Ray& r, const Hittable& world, i32 depth) {
+    if (depth <= 0)
+      return Color3(0.0, 0.0, 0.0);
+    
     HitRecord hr;
-    if (scene.hit(r, 0.0, F64_INF, &hr))
-      return 0.5 * (hr.normal + Vec3(1.0, 1.0, 1.0));
+    if (world.hit(r, 0.001, F64_INF, &hr)) {
+      Point3 target = hr.p + random_vec3_in_hemisphere(hr.normal);
+      return 0.5 * ray_color(Ray(hr.p, target - hr.p), world, depth-1);
+    }
     
     Vec3 unit_dir = normalize(r.dir);
     f64 t = 0.5 * (unit_dir.y + 1.0);
@@ -22,9 +28,10 @@ namespace sim {
   }
   
   void write_color3(FILE* out, const Color3& c) {
-    i32 ir = (i32)(256.0 * clamp(c.x, 0.0, 0.999));
-    i32 ig = (i32)(256.0 * clamp(c.y, 0.0, 0.999));
-    i32 ib = (i32)(256.0 * clamp(c.z, 0.0, 0.999));
+    // sqrt <=> gamma=2.0
+    i32 ir = (i32)(256.0 * clamp(sqrt(c.x), 0.0, 0.999));
+    i32 ig = (i32)(256.0 * clamp(sqrt(c.y), 0.0, 0.999));
+    i32 ib = (i32)(256.0 * clamp(sqrt(c.z), 0.0, 0.999));
     fprintf(out, "%d %d %d\n", ir, ig, ib);
   }
 }
@@ -51,7 +58,7 @@ int main() {
       for (i32 i = 0; i < PIXEL_SAMPLES; ++i) {
         f64 u = ((f64)x + random_f64()) / (IMG_W-1);
         f64 v = ((f64)y + random_f64()) / (IMG_H-1);
-        pixel += ray_color(camera.cast_ray(u, v), world);
+        pixel += ray_color(camera.cast_ray(u, v), world, MAX_DEPTH);
       }
       pixel /= PIXEL_SAMPLES;
       write_color3(stdout, pixel);
